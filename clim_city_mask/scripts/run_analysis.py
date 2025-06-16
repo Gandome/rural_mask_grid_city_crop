bf8e3abcb922c67a23c2a67b680b1658c593db54
Gandome <28142032+Gandome@users.noreply.github.comlist>
2025-06-16 19:18:23 +0200

    Remove ignored files and update .gitignore

--- clim_city_mask/scripts/run_analysis.py
+++ clim_city_mask/scripts/run_analysis.py
@@ -2,15 +2,15 @@ from climate_city_stats.io import load_city_polygons, load_climate_data
 from climate_city_stats.utils import kelvin_humidity_convert, sanitize_filename, format_units, build_grid_polygons, group_time_data
 from climate_city_stats.stats import compute_city_stats
 
-file_gpkg = "/home/quenumm/Documents/Scripts/Python/github/climate_city_mask/data/GHS_UCDB_REGION_EUROPE_R2024A.gpkg"
-data_file = "/home/quenumm/Documents/Scripts/Python/github/climate_city_mask/data/tasmax_ALPX-3_CNRM-ESM2-1_historical_r1i1p1f2_CNRM-MF_CNRM-AROME46t1_v1-r1_1hr_JJAS_1995.nc"
-target_cities = ["Paris", "Berlin", "Rome"]
-variable = "tas"
+file_gpkg = "/home/quenumm/Documents/Scripts/Python/github/UHI_grid_mask-city/rural_mask_grid_city_crop/clim_city_mask/data/GHS_UCDB_REGION_EUROPE_R2024A.gpkg"
+data_file = "/home/quenumm/Documents/data/data_lea/2022/day/output/Urban_Heat_Island_data_NFR2.5_ERA5_eval_CNRM-AROME46t1_v1_1hr_2022_000001.nc"
+target_cities = ["Paris"]# , "Berlin", "Rome"]
+variable = "UHI_LR100"
 time_period=["seasonal"] #, 'seasonal', 'daily', 'monthly'
 cmap='viridis'
 
 for time_period in time_period:
-    plot_dir="plots/{time_period}"
+    plot_dir=f"plots/{time_period}"
     output_csv=f"results/city_statistics_{time_period}.csv"
     netcdf_dir=f"outputs/masked_vals_{time_period}"
 
